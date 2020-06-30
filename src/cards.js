06b06d133e3dae4bc56713a24437cc8f416536e8
jakehsiao <1499975101@qq.com>
2020-06-30 23:32:09 +0800

    Minor changes.

--- src/cards.js
+++ src/cards.js
@@ -538,10 +538,10 @@ export var CARDS = [
   
   {
     name:"艾雅法拉",
-    cost:6,
-    atk:6,
-    hp:4,
-    mine:4,
+    cost:4,
+    atk:4,
+    hp:3,
+    mine:3,
     block:0,
     desc:"采掘：触发场上所有干员的“采掘:”效果",
     illust:"http://ak.mooncell.wiki/images/c/c0/%E7%AB%8B%E7%BB%98_%E8%89%BE%E9%9B%85%E6%B3%95%E6%8B%89_1.png",
