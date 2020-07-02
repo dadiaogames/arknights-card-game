b2310fcd093a365ed051c7b4068b40c5e53b3c61
jakehsiao <1499975101@qq.com>
2020-07-02 19:57:49 +0800

    Minor changes.

--- src/TitleScreen.js
+++ src/TitleScreen.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './TitleScreen.css';
 
 export const TitleScreen = (props) => (
-  <div style={{textAlign: "center", marginTop:"50%"}}>
+  <div style={{textAlign: "center", marginTop:"40%"}}>
     <h2>明日方舟: 采掘行动</h2>
+    <div >
+      制作: <a href="https://space.bilibili.com/8492192/">大雕游戏</a>
+    </div>
     <button className="title-screen-button" onClick={props.enterGame}>进入游戏</button><br/>
     <button className="title-screen-button" onClick={props.checkRule}>查看规则</button><br/>
     <button className="title-screen-button" onClick={props.checkDeck}>干员图鉴</button><br/>
