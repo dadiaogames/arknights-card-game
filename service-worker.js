058071720c478d4acffcc53cc35f5c0633d13765
jakehsiao <1499975101@qq.com>
2020-09-15 13:38:26 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arknights-card-game/precache-manifest.df67951fbc7ce837963f34294d39379c.js"
+  "/arknights-card-game/precache-manifest.ac24840e81450b5127586e73be940033.js"
 );
 
 self.addEventListener('message', (event) => {
