d0d0a4657f68ef233504ae8e9c80018c2bbd8b7c
jakehsiao <1499975101@qq.com>
2020-11-12 20:48:42 +0800

    Minor changes.

--- src/TitleScreen.js
+++ src/TitleScreen.js
@@ -7,9 +7,9 @@ export const TitleScreen = (props) => (
     <div >
       制作: <a href="https://space.bilibili.com/8492192/dynamic/">大雕游戏</a>
     </div>
-    <button className="title-screen-button" onClick={props.enterGame}>进入游戏</button><br/>
-    <button className="title-screen-button" onClick={props.checkRule}>玩法教学</button><br/>
-    <button className="title-screen-button" onClick={props.checkDeck}>干员图鉴</button><br/>
+    <button className="title-screen-button primary" onClick={props.enterGame}>进入游戏</button><br/>
+    <button className="title-screen-button primary" onClick={props.checkRule}>玩法教学</button><br/>
+    <button className="title-screen-button primary" onClick={props.checkDeck}>干员图鉴</button><br/>
   </div>
 );
 
