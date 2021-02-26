91673a7ed5e44f97c71fbaa73e850fefc0686d1c
jakehsiao <1499975101@qq.com>
2021-02-27 07:41:05 +0800

    Minor changes.

--- src/orders.js
+++ src/orders.js
@@ -113,7 +113,7 @@ export const ORDERS = [
   
   {
     requirements: [3,0,0,0],
-    score: 4,
+    score: 3,
     reward: 1,
     desc: (<span>1费→{material_icons[2]}{material_icons[2]}</span>),
     // cost: [0,0,1,0],
@@ -125,7 +125,7 @@ export const ORDERS = [
   },
   {
     requirements: [0,3,0,0],
-    score: 4,
+    score: 3,
     reward: 2,
     desc: (<span>1费→{material_icons[0]}{material_icons[0]}</span>),
     // cost: [1,0,0,0],
@@ -138,7 +138,7 @@ export const ORDERS = [
   },
   {
     requirements: [0,0,3,0],
-    score: 4,
+    score: 3,
     reward: 0,
     desc: (<span>1费→{material_icons[1]}{material_icons[1]}</span>),
     // cost: [0,1,0,0],
@@ -287,7 +287,7 @@ export const ORDERS = [
   
   {
     requirements: [3,0,0,0],
-    score: 3,
+    score: 2,
     reward: 1,
     desc: (<span>{material_icons[2]} → 2分</span>),
     cost: [0,0,1,0],
@@ -297,7 +297,7 @@ export const ORDERS = [
   },
   {
     requirements: [0,3,0,0],
-    score: 3,
+    score: 2,
     reward: 2,
     desc: (<span>{material_icons[0]} → 2分</span>),
     cost: [1,0,0,0],
@@ -308,7 +308,7 @@ export const ORDERS = [
   },
   {
     requirements: [0,0,3,0],
-    score: 3,
+    score: 2,
     reward: 0,
     desc: (<span>{material_icons[1]} → 2分</span>),
     cost: [0,1,0,0],
