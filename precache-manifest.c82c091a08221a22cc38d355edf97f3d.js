8265ec80d99f506a0bf9c3927e614fee67a61035
jakehsiao <1499975101@qq.com>
2021-04-15 15:35:50 +0800

    Updates

--- precache-manifest.c82c091a08221a22cc38d355edf97f3d.js
+++ precache-manifest.c82c091a08221a22cc38d355edf97f3d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b6cfbc8e7b0e720720db6937a39eb0b6",
+    "revision": "0345130e837cdc7b2b1808e898091165",
     "url": "/arknights-card-game/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arknights-card-game/static/css/2.5c17b9f1.chunk.css"
   },
   {
-    "revision": "bdf08646a54d02a7a014",
+    "revision": "6058af8affb82efb2fd7",
     "url": "/arknights-card-game/static/css/main.377a8911.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arknights-card-game/static/js/2.c069a43b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bdf08646a54d02a7a014",
-    "url": "/arknights-card-game/static/js/main.7d4ef947.chunk.js"
+    "revision": "6058af8affb82efb2fd7",
+    "url": "/arknights-card-game/static/js/main.f7ac41fd.chunk.js"
   },
   {
     "revision": "e689b0d9f31f241d8b2d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arknights-card-game/precache-manifest.0d1df77d78f782673395d61b08ba9ab1.js"
+  "/arknights-card-game/precache-manifest.c82c091a08221a22cc38d355edf97f3d.js"
 );
 
 self.addEventListener('message', (event) => {
